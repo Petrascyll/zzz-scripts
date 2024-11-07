2803669de98e99e955f40fd0c2f93b90c9dc681e
petrascyll <124032137+Petrascyll@users.noreply.github.com>
2024-11-06 23:22:37 -0500

    1.3B - Lucy Fix and Piper Body 2048p Diffuse

--- zzz_fix.py
+++ zzz_fix.py
@@ -2693,128 +2693,155 @@ hash_commands = {
 
 
     # MARK: Lucy
-    '198e99d7': [(log, ('1.0: Lucy Hair IB Hash',)),     (add_ib_check_if_missing,)],
+    '69ad9d08': [(log, ('1.3: Lucy Hair IB Hash',)),     (add_ib_check_if_missing,)],
     '272dd7f6': [(log, ('1.0: Lucy Snout IB Hash',)),    (add_ib_check_if_missing,)],
     '9b6370f6': [(log, ('1.0: Lucy Belt IB Hash',)),     (add_ib_check_if_missing,)],
-    'e0ad50ed': [(log, ('1.0: Lucy Body IB Hash',)),     (add_ib_check_if_missing,)],
+    'be5f4c7d': [(log, ('1.3: Lucy Body IB Hash',)),     (add_ib_check_if_missing,)],
     '1fe6e084': [(log, ('1.0: Lucy RedCloth IB Hash',)), (add_ib_check_if_missing,)],
     'a0ed04de': [(log, ('1.0: Lucy Helmet IB Hash',)),   (add_ib_check_if_missing,)],
-    'fca15ccb': [(log, ('1.0: Lucy Head IB Hash',)),     (add_ib_check_if_missing,)],
+    'df3e3965': [(log, ('1.3: Lucy Head IB Hash',)),     (add_ib_check_if_missing,)],
 
-
-    '483b418a': [
-        (log,                           ('1.0: Lucy HeadA Diffuse 1024p Hash',)),
-        (add_section_if_missing,        ('fca15ccb', 'Lucy.Head.IB', 'match_priority = 0\n')),
-        (multiply_section_if_missing,   ('2a6df536', 'Lucy.HeadA.Diffuse.2048')),
+    '5315f036': [(log, ('1.2 -> 1.3: Lucy Hair Blend Hash',)),    (update_hash, ('a37c7537',))],
+    '751e21a5': [(log, ('1.2 -> 1.3: Lucy Hair Texcoord Hash',)), (update_hash, ('c8810832',))],
+    '198e99d7': [
+        (log, ('1.2 -> 1.3: Lucy Hair IB Hash',)),
+        (update_hash, ('69ad9d08',)),
+        (transfer_indexed_sections, {
+            'src_indices': ['0', '-1'],
+            'trg_indices': ['0', '5253'],
+        })
     ],
-    '2a6df536': [
-        (log,                           ('1.0: Lucy HeadA Diffuse 2048p Hash',)),
-        (add_section_if_missing,        ('fca15ccb', 'Lucy.Head.IB', 'match_priority = 0\n')),
-        (multiply_section_if_missing,   ('483b418a', 'Lucy.HeadA.Diffuse.1024')),
+
+    '5da9dafc': [(log, ('1.2 -> 1.3: Lucy Body Position Hash',)), (update_hash, ('246b93e2',))],
+    'b94b02e8': [(log, ('1.2 -> 1.3: Lucy Body Blend Hash',)),    (update_hash, ('66948a0f',))],
+    '00f11ea6': [(log, ('1.2 -> 1.3: Lucy Body Texcoord Hash',)), (update_hash, ('f60dbb9e',))],
+    'e0ad50ed': [(log, ('1.2 -> 1.3: Lucy Body IB Hash',)),       (update_hash, ('be5f4c7d',))],
+
+    'fca15ccb': [(log, ('1.2 -> 1.3: Lucy Head IB Hash',)),       (update_hash, ('df3e3965',))],
+
+
+    '483b418a': [(log, ('1.2 -> 1.3: Lucy HeadA Diffuse 1024p Hash',)), (update_hash, ('2578d35b',))],
+    '2a6df536': [(log, ('1.2 -> 1.3: Lucy HeadA Diffuse 1024p Hash',)), (update_hash, ('4e2d5baa',))],
+
+    '2578d35b': [
+        (log,                           ('1.3: Lucy HeadA Diffuse 1024p Hash',)),
+        (add_section_if_missing,        (('df3e3965', 'fca15ccb'), 'Lucy.Head.IB', 'match_priority = 0\n')),
+        (multiply_section_if_missing,   (('4e2d5baa', '2a6df536'), 'Lucy.HeadA.Diffuse.2048')),
+    ],
+    '4e2d5baa': [
+        (log,                           ('1.3: Lucy HeadA Diffuse 2048p Hash',)),
+        (add_section_if_missing,        (('df3e3965', 'fca15ccb'), 'Lucy.Head.IB', 'match_priority = 0\n')),
+        (multiply_section_if_missing,   (('2578d35b', '483b418a'), 'Lucy.HeadA.Diffuse.1024')),
     ],
 
 
-    'aa513afa': [
-        (log,                           ('1.0: Lucy HairA, SnoutA, BeltA Diffuse 2048p Hash',)),
-        (add_section_if_missing,        ('198e99d7', 'Lucy.Hair.IB', 'match_priority = 0\n')),
+    'b50eb71c': [(log, ('1.2 -> 1.3: Lucy HairA, SnoutA, BeltA Diffuse 1024p Hash',)),     (update_hash, ('753baa45',))],
+    'd1241cfc': [(log, ('1.2 -> 1.3: Lucy HairA, SnoutA, BeltA MaterialMap 1024p Hash',)), (update_hash, ('368f931c',))],
+
+    'aa513afa': [(log, ('1.2 -> 1.3: Lucy HairA, SnoutA, BeltA Diffuse 2048p Hash',)),     (update_hash, ('0fa60fe1',))],
+    '919b608c': [(log, ('1.2 -> 1.3: Lucy HairA, SnoutA, BeltA MaterialMap 2048p Hash',)), (update_hash, ('068aba7f',))],
+
+    '0fa60fe1': [
+        (log,                           ('1.3: Lucy HairA, SnoutA, BeltA Diffuse 2048p Hash',)),
+        (add_section_if_missing,        (('69ad9d08', '198e99d7'), 'Lucy.Hair.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('272dd7f6', 'Lucy.Snout.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('9b6370f6', 'Lucy.Belt.IB', 'match_priority = 0\n')),
-        (multiply_section_if_missing,   ('b50eb71c', 'Lucy.HairA.Diffuse.1024')),
+        (multiply_section_if_missing,   (('753baa45', 'b50eb71c'), 'Lucy.HairA.Diffuse.1024')),
     ],
-    'b50eb71c': [
-        (log,                           ('1.0: Lucy HairA, SnoutA, BeltA Diffuse 1024p Hash',)),
-        (add_section_if_missing,        ('198e99d7', 'Lucy.Hair.IB', 'match_priority = 0\n')),
+    '753baa45': [
+        (log,                           ('1.3: Lucy HairA, SnoutA, BeltA Diffuse 1024p Hash',)),
+        (add_section_if_missing,        (('69ad9d08', '198e99d7'), 'Lucy.Hair.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('272dd7f6', 'Lucy.Snout.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('9b6370f6', 'Lucy.Belt.IB', 'match_priority = 0\n')),
-        (multiply_section_if_missing,   ('aa513afa', 'Lucy.HairA.Diffuse.2048')),
+        (multiply_section_if_missing,   (('0fa60fe1', 'aa513afa'), 'Lucy.HairA.Diffuse.2048')),
     ],
     '1a3b30ba': [
         (log,                           ('1.0: Lucy HairA, SnoutA, BeltA LightMap 2048p Hash',)),
-        (add_section_if_missing,        ('198e99d7', 'Lucy.Hair.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('69ad9d08', '198e99d7'), 'Lucy.Hair.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('272dd7f6', 'Lucy.Snout.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('9b6370f6', 'Lucy.Belt.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('810c0878', 'Lucy.HairA.LightMap.1024')),
     ],
     '810c0878': [
         (log,                           ('1.0: Lucy HairA, SnoutA, BeltA LightMap 1024p Hash',)),
-        (add_section_if_missing,        ('198e99d7', 'Lucy.Hair.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('69ad9d08', '198e99d7'), 'Lucy.Hair.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('272dd7f6', 'Lucy.Snout.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('9b6370f6', 'Lucy.Belt.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('1a3b30ba', 'Lucy.HairA.LightMap.2048')),
     ],
-    '919b608c': [
-        (log,                           ('1.0: Lucy HairA, SnoutA, BeltA MaterialMap 2048p Hash',)),
-        (add_section_if_missing,        ('198e99d7', 'Lucy.Hair.IB', 'match_priority = 0\n')),
+    '068aba7f': [
+        (log,                           ('1.3: Lucy HairA, SnoutA, BeltA MaterialMap 2048p Hash',)),
+        (add_section_if_missing,        (('69ad9d08', '198e99d7'), 'Lucy.Hair.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('272dd7f6', 'Lucy.Snout.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('9b6370f6', 'Lucy.Belt.IB', 'match_priority = 0\n')),
-        (multiply_section_if_missing,   ('d1241cfc', 'Lucy.HairA.MaterialMap.1024')),
+        (multiply_section_if_missing,   (('368f931c', 'd1241cfc'), 'Lucy.HairA.MaterialMap.1024')),
     ],
-    'd1241cfc': [
-        (log,                           ('1.0: Lucy HairA, SnoutA, BeltA MaterialMap 1024p Hash',)),
-        (add_section_if_missing,        ('198e99d7', 'Lucy.Hair.IB', 'match_priority = 0\n')),
+    '368f931c': [
+        (log,                           ('1.3: Lucy HairA, SnoutA, BeltA MaterialMap 1024p Hash',)),
+        (add_section_if_missing,        (('69ad9d08', '198e99d7'), 'Lucy.Hair.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('272dd7f6', 'Lucy.Snout.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('9b6370f6', 'Lucy.Belt.IB', 'match_priority = 0\n')),
-        (multiply_section_if_missing,   ('919b608c', 'Lucy.HairA.MaterialMap.2048')),
+        (multiply_section_if_missing,   (('068aba7f', '919b608c'), 'Lucy.HairA.MaterialMap.2048')),
     ],
     'edcb9661': [
         (log,                           ('1.0: Lucy HairA, SnoutA, BeltA NormalMap 2048p Hash',)),
-        (add_section_if_missing,        ('198e99d7', 'Lucy.Hair.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('69ad9d08', '198e99d7'), 'Lucy.Hair.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('272dd7f6', 'Lucy.Snout.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('9b6370f6', 'Lucy.Belt.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('9114c7c7', 'Lucy.HairA.NormalMap.1024')),
     ],
     '9114c7c7': [
         (log,                           ('1.0: Lucy HairA, SnoutA, BeltA NormalMap 1024p Hash',)),
-        (add_section_if_missing,        ('198e99d7', 'Lucy.Hair.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('69ad9d08', '198e99d7'), 'Lucy.Hair.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('272dd7f6', 'Lucy.Snout.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('9b6370f6', 'Lucy.Belt.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('edcb9661', 'Lucy.HairA.NormalMap.2048')),
     ],
     '474c7aa2': [
         (log,                           ('1.0: Lucy BodyA, RedClothA Diffuse 2048p Hash',)),
-        (add_section_if_missing,        ('e0ad50ed', 'Lucy.Body.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('be5f4c7d', 'e0ad50ed'), 'Lucy.Body.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('1fe6e084', 'Lucy.RedCloth.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('f810e7ac', 'Lucy.BodyA.Diffuse.1024')),
     ],
     'f810e7ac': [
         (log,                           ('1.0: Lucy BodyA, RedClothA Diffuse 1024p Hash',)),
-        (add_section_if_missing,        ('e0ad50ed', 'Lucy.Body.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('be5f4c7d', 'e0ad50ed'), 'Lucy.Body.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('1fe6e084', 'Lucy.RedCloth.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('474c7aa2', 'Lucy.BodyA.Diffuse.2048')),
     ],
     '855d9fa3': [
         (log,                           ('1.0: Lucy BodyA, RedClothA LightMap 2048p Hash',)),
-        (add_section_if_missing,        ('e0ad50ed', 'Lucy.Body.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('be5f4c7d', 'e0ad50ed'), 'Lucy.Body.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('1fe6e084', 'Lucy.RedCloth.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('e89f7814', 'Lucy.BodyA.LightMap.1024')),
     ],
     'e89f7814': [
         (log,                           ('1.0: Lucy BodyA, RedClothA LightMap 1024p Hash',)),
-        (add_section_if_missing,        ('e0ad50ed', 'Lucy.Body.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('be5f4c7d', 'e0ad50ed'), 'Lucy.Body.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('1fe6e084', 'Lucy.RedCloth.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('855d9fa3', 'Lucy.BodyA.LightMap.2048')),
     ],
     '1fd24fd8': [
         (log,                           ('1.0: Lucy BodyA, RedClothA MaterialMap 2048p Hash',)),
-        (add_section_if_missing,        ('e0ad50ed', 'Lucy.Body.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('be5f4c7d', 'e0ad50ed'), 'Lucy.Body.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('1fe6e084', 'Lucy.RedCloth.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('86ca6cfd', 'Lucy.BodyA.MaterialMap.1024')),
     ],
     '86ca6cfd': [
         (log,                           ('1.0: Lucy BodyA, RedClothA MaterialMap 1024p Hash',)),
-        (add_section_if_missing,        ('e0ad50ed', 'Lucy.Body.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('be5f4c7d', 'e0ad50ed'), 'Lucy.Body.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('1fe6e084', 'Lucy.RedCloth.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('1fd24fd8', 'Lucy.BodyA.MaterialMap.2048')),
     ],
     '463b4f55': [
         (log,                           ('1.0: Lucy BodyA, RedClothA NormalMap 2048p Hash',)),
-        (add_section_if_missing,        ('e0ad50ed', 'Lucy.Body.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('be5f4c7d', 'e0ad50ed'), 'Lucy.Body.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('1fe6e084', 'Lucy.RedCloth.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('1711cafd', 'Lucy.BodyA.NormalMap.1024')),
     ],
     '1711cafd': [
         (log,                           ('1.0: Lucy BodyA, RedClothA NormalMap 1024p Hash',)),
-        (add_section_if_missing,        ('e0ad50ed', 'Lucy.Body.IB', 'match_priority = 0\n')),
+        (add_section_if_missing,        (('be5f4c7d', 'e0ad50ed'), 'Lucy.Body.IB', 'match_priority = 0\n')),
         (add_section_if_missing,        ('1fe6e084', 'Lucy.RedCloth.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   ('463b4f55', 'Lucy.BodyA.NormalMap.2048')),
     ],
@@ -3355,17 +3382,17 @@ hash_commands = {
 
 
     '621564e5': [(log, ('1.2 -> 1.3: Piper BodyA Diffuse 1024p Hash',)), (update_hash, ('b450949d',))],
-    # TODO get piper diffuse 2048p hash
+    'b4b74e7e': [(log, ('1.2 -> 1.3: Piper BodyA Diffuse 2048p Hash',)), (update_hash, ('fed40302',))],
 
-    'b4b74e7e': [
-        (log,                           ('1.0: Piper BodyA Diffuse 2048p Hash',)),
+    'fed40302': [
+        (log,                           ('1.3: Piper BodyA Diffuse 2048p Hash',)),
         (add_section_if_missing,        ('585da98b', 'Piper.Body.IB', 'match_priority = 0\n')),
         (multiply_section_if_missing,   (('b450949d', '621564e5'), 'Piper.BodyA.Diffuse.1024')),
     ],
     'b450949d': [
-        (log,                           ('1.0: Piper BodyA Diffuse 1024p Hash',)),
+        (log,                           ('1.3: Piper BodyA Diffuse 1024p Hash',)),
         (add_section_if_missing,        ('585da98b', 'Piper.Body.IB', 'match_priority = 0\n')),
-        (multiply_section_if_missing,   ('b4b74e7e', 'Piper.BodyA.Diffuse.2048')),
+        (multiply_section_if_missing,   (('fed40302', 'b4b74e7e'), 'Piper.BodyA.Diffuse.2048')),
     ],
     '9cc2aaa0': [
         (log,                           ('1.0: Piper BodyA LightMap 2048p Hash',)),
